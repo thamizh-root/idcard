d63d8bed72a8e6a9e0ecd4298624c449813c822a
thamizh-root <58970160+thamizh-root@users.noreply.github.com>
2025-04-23 22:29:53 +0530

    added two blogs

--- app/blog/blog-data.tsx
+++ app/blog/blog-data.tsx
@@ -8,8 +8,20 @@ export interface Blogs {
 export const blogs: Blogs[] = [
   {
     title: "Debugging React Internals",
-    year: 2024,
-    publishedAt: "Apr 15, 2025",
+    year: 2025,
+    publishedAt: "Apr, 2025",
     url: "https://watermark.hashnode.dev/debugging-react-internals-why-i-started-cloning-functions-and-objects-seriously",
+  },
+  {
+    title: "The Unsung Hero",
+    year: 2025,
+    publishedAt: "Apr, 2025",
+    url: "https://watermark.hashnode.dev/requestidlecallback-the-unsung-hero-of-web-performance",
+  },
+  {
+    title: "Understanding Promise",
+    year: 2025,
+    publishedAt: "Apr, 2025",
+    url: "https://watermark.hashnode.dev/understanding-promiseall-and-the-power-of-javascript-promises",
   }
 ];
