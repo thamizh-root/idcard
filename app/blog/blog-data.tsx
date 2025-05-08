45e9ed970502462f1f28b29ea102d0bdaa2376e2
thamizh-root <58970160+thamizh-root@users.noreply.github.com>
2025-05-08 13:39:21 +0530

    feat: added new redux blog

--- app/blog/blog-data.tsx
+++ app/blog/blog-data.tsx
@@ -7,16 +7,22 @@ export interface Blogs {
 
 export const blogs: Blogs[] = [
   {
-    title: "Debugging React Internals",
+    title: "Build Your Own Redux ✨",
     year: 2025,
-    publishedAt: "Apr, 2025",
-    url: "https://watermark.hashnode.dev/debugging-react-internals-why-i-started-cloning-functions-and-objects-seriously",
+    publishedAt: "May, 2025",
+    url: "https://watermark.hashnode.dev/not-just-another-redux-article-learning-redux-from-its-source-code",
   },
   {
     title: "The Unsung Hero",
     year: 2025,
     publishedAt: "Apr, 2025",
     url: "https://watermark.hashnode.dev/requestidlecallback-the-unsung-hero-of-web-performance",
+  },
+  {
+    title: "Debugging React Internals",
+    year: 2025,
+    publishedAt: "Apr, 2025",
+    url: "https://watermark.hashnode.dev/debugging-react-internals-why-i-started-cloning-functions-and-objects-seriously",
   },
   {
     title: "Understanding Promise",
